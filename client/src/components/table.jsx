78b20cb2abe6cf7a0e9c08f3db7f11444794e9ed
tushort <samquioh@gmail.com>
2023-09-22 01:02:37 -0700

    add the sampledata to table

--- client/src/components/table.jsx
+++ client/src/components/table.jsx
@@ -1,11 +1,11 @@
 
 
 const sampledata = [
-{ Id: 6, Date: '3/5/15,', Individual: 'Bangled Cobra' , Healthy: 'true', Email: 'Dr.Parl@gmail.com'},
-{ Id: 7, Date: '1/5/20,', Individual: 'Striped RoadRunner' , Healthy: true, Email: 'Tracy8l@gmail.com'},
-{ Id: 8, Date: '6/9/18,', Individual: 'Bangled Cobra' , Healthy: true, Email: 'Dr.Parl@gmail.com'},
-{ Id: 9, Date: '5/30/15,', Individual: 'Bangled Cobra' , Healthy: false, Email: 'Dr.Xerxes@gmail.com'},
-{ Id: 10, Date: '4/28/22,', Individual: 'Bangled Cobra' , Healthy: true, Email: 'Dr.Johnson@gmail.com'}
+{ Id: 6, Date: '3/5/15,', Individual: 'Bangled Cobra' ,     Healthy: 'true',     Email: 'Dr.Parl@gmail.com'},
+{ Id: 7, Date: '1/5/20,', Individual: 'Striped RoadRunner',  Healthy: 'true',    Email: 'Tracy8l@gmail.com'},
+{ Id: 8, Date: '6/9/18,', Individual: 'Bangled Cobra' ,     Healthy: 'true',     Email: 'Dr.Parl@gmail.com'},
+{ Id: 9, Date: '5/30/15,', Individual: 'Howler Monkey' ,    Healthy: 'false',    Email: 'Dr.Xerxes@gmail.com'},
+{ Id: 10, Date: '4/28/22,', Individual: 'Bangled Cobra' ,   Healthy: 'true',     Email: 'Dr.Johnson@gmail.com'}
 
 ]
  
@@ -15,12 +15,9 @@ function Tablebox(){
             <table>
                 <tr className="tr">
                     <th>Id</th>
-                    <br />
                     <th>Date</th>
                     <th>Individual</th>
                     <th>Healthy?</th>
-                    <br />
-                    
                     <th>Email Address</th>
                     
 
