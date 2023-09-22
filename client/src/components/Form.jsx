3da2b54a6c8e6f1fea29f45755056a41bb9b32cf
tushort <samquioh@gmail.com>
2023-09-21 20:01:38 -0700

    adds onsubmit function passed as a prop, takes in allAppInfo.

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -9,23 +9,42 @@ const specConStatus = useRef();
 
 // use ref initialization --- Individuals   
 const indivNickName = useRef();
-const indivSpecies = useRef();
-
-
+const indivSpecies = useRef(); //const specSciName = useRef();
 
 // use ref initialization --- Sightings
-
-
+const sightTime = useRef();
+const sightIndividual= useRef();
+const sightLocation = useRef();
+const sightHealth = useRef();
+const userEmail = useRef();
 
 
 //new function called handleSubmit that takes an event object as a parameter.
- 
-
-
+ const handleSubmit = (event) => {
+event.preventDefault();
 /*
 new object called allUserInfo that contains the values
  of all of the form fields. The current?.value syntax is used to access the value of the form field, but only if the field is not null or undefined.
 */
+const allAppInfo ={
+  speciesCommonName:specCommonName.current?.value,
+  speciesScientificName:specSciName.current?.value,
+  speciesInTheWild:specInWild?.value,
+  speciesConservationStatus:specConStatus.current?.value,
+  sightingTime:sightTime.current?.value,
+  sightingIndividual:sightIndividual.current?.value,
+  sightLocation:sightLocation.current?.value,
+  sightHealth:sightHealth.current?.value,
+  userEmail:userEmail.current?.value,
+  individualNickname:indivNickName.current?.value,
+  individualSpecies:indivSpecies.current?.value,
+};
+// onSubmit() function that was passed to the handleSubmit function as a prop. The allUserInfo object is passed to the onSubmit()
+props.onSubmit(allAppInfo)
+ }
+
+
+
     return (
 
        <div>{sightings.length >0 ? 
