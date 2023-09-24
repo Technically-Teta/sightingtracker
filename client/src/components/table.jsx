97dfaeba76fe5205a9ae65490da8c0d23719497d
tushort <samquioh@gmail.com>
2023-09-23 19:38:29 -0700

    deletes sample data table

--- client/src/components/table.jsx
+++ client/src/components/table.jsx
@@ -1,13 +1,4 @@
 
-
-const sampledata = [
-{ Id: 6, Date: '3/5/15,', Individual: 'Bangled Cobra' ,     Healthy: 'true',     Email: 'Dr.Parl@gmail.com'},
-{ Id: 7, Date: '1/5/20,', Individual: 'Striped RoadRunner',  Healthy: 'true',    Email: 'Tracy8l@gmail.com'},
-{ Id: 8, Date: '6/9/18,', Individual: 'Bangled Cobra' ,     Healthy: 'true',     Email: 'Dr.Parl@gmail.com'},
-{ Id: 9, Date: '5/30/15,', Individual: 'Howler Monkey' ,    Healthy: 'false',    Email: 'Dr.Xerxes@gmail.com'},
-{ Id: 10, Date: '4/28/22,', Individual: 'Bangled Cobra' ,   Healthy: 'true',     Email: 'Dr.Johnson@gmail.com'}
-
-]
  
 function Tablebox(){
     return (
